ee85ab077bc765f46dd017764bcc0b55d8e5a00e
Sponge Guy <realevan03@gmail.com>
2023-06-14 18:38:38 -0400

    added set and clear buttons

--- js/mainboard.js
+++ js/mainboard.js
@@ -1,10 +1,26 @@
+var mainConfig = 
+{
+    draggable: true,
+    dropOffBoard: 'snapback',
+    sparePieces: false,
+    showNotation: false,
+    height: mainBoardHeight + 'px'
+}
+
 var mainBoardHeight = document.querySelector('.chess-window').clientHeight;
 
 // change this later
-var mainBoard = ChessBoard('mainboard',
+var mainBoard = ChessBoard('mainboard', mainConfig);
+
+function setBoardButton()
 {
-    draggable: true,
-    dropOffBoard: 'trash',
-    sparePieces: true,
-    height: mainBoardHeight + 'px'
-});
+    mainBoard.start();
+}
+
+function clearBoardButton()
+{
+    mainBoard.clear();
+}
+
+document.getElementById('setButton').addEventListener('click', setBoardButton);
+document.getElementById('clearButton').addEventListener('click', clearBoardButton);
