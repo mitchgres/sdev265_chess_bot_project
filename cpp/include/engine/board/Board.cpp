7baccb0e41eeb44300a7185d003505755feae3d3
Mitchell <mgresham4@ivytech.edu>
2023-07-07 22:04:21 -0400

    Comments :).

--- cpp/include/engine/board/Board.cpp
+++ cpp/include/engine/board/Board.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file Board.cpp
+ * @author Mitchell M. Gresham
+ * @brief Representation of a Chess Board.
+ * @version 0.1
+ * @date 2023-07-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <iostream>
 
 #include "Board.h"
