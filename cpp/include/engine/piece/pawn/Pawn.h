7c2148877adb37444e78542ce2545d1453b9c506
joshuabowles <107223002+joshuabowles@users.noreply.github.com>
2023-07-10 11:49:53 -0400

    Update Pawn.h

--- cpp/include/engine/piece/pawn/Pawn.h
+++ cpp/include/engine/piece/pawn/Pawn.h
@@ -1,16 +1,22 @@
+#ifndef __PAWN_H__
+#define __PAWN_H__
+
 #include "../Piece.h"
 
-class Pawn: public Piece {
-    public:
-        inline PIECE _getID() const;
-        Position* _getLegalMoves() const;
-        Position* _getLegalAttackMoves() const;
-        Position* _getAllLegalMoves() const;
-        
-    private:
-        bool _isCapturePossible(const Piece&) const;
-        bool _isMovePossible(const Position&) const;
-        Position* _getUnprocessedMoves() const;
-        Position* _getUnprocessedAttackMoves() const;
-        Position* _processMoves(const Position*&) const;
+class Pawn : public Piece {
+public:
+    Pawn(Position);
+    PIECE _getID() const override;
+    Position* _getLegalMoves() const override;
+    Position* _getLegalAttackMoves() const override;
+    Position* _getAllLegalMoves() const override;
+
+private:
+    bool _canTake(const Piece&) const override;
+    bool _canMove(const Position&) const override;
+    Position* _getUnprocessedMoves() const override;
+    Position* _getUnprocessedAttackMoves() const override;
+    Position* _processMoves(const Position*&) const override;
 };
+
+#endif
