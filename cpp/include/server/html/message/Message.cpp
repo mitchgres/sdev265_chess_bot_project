35aba5e2209c2861c434c2b1f43ab12f9c41cb08
Mitchell <mgresham4@ivytech.edu>
2023-07-14 18:02:58 -0400

    Updated Message

---
+++ cpp/include/server/html/message/Message.cpp
@@ -0,0 +1,19 @@
+#include "Message.h"
+
+
+Message::Message(const Piece&, const Position&)
+{
+    // Conversion from engine data types to FEN Notation. 
+}
+
+
+Message::~Message()
+{
+
+}
+
+
+std::string Message::_get() const
+{
+
+}
