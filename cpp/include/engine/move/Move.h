c412c42a06b80320eec14b533904682bb6ac1091
Mitchell <mgresham4@ivytech.edu>
2023-07-14 18:07:31 -0400

    Fixed Move

--- cpp/include/engine/move/Move.h
+++ cpp/include/engine/move/Move.h
@@ -2,8 +2,15 @@
 #include "../position/Position.h"
 
 /// @brief Representation of the movement that a piece will make on the board. Previous & New Location, and the moved piece.
-struct Move {
-    Piece _piece;
-    Position _old_position;
-    Position _new_position;
-}
+class Move 
+{
+    public: 
+        Move(const Piece*, const Position&, const Position&);
+        inline Piece* _get_piece_ptr() const;
+        inline Position _get_old_position() const;
+        inline Position _get_new_position() const;
+    private:   
+        Piece* _piece; // Pointer is needed because it's abstract superclass.
+        Position _old_position;
+        Position _new_position;
+};
